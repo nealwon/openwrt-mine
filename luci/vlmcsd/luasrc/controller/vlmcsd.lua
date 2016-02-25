a0113f18c793c56fa2fb6116b1400fab812b71e4
nealwon <nealwon@aol.com>
2016-02-25 19:48:23 +0800

    fix uci words

--- luci/vlmcsd/luasrc/controller/vlmcsd.lua
+++ luci/vlmcsd/luasrc/controller/vlmcsd.lua
@@ -1,8 +1,9 @@
+require("nixio.fs")
 module("luci.controller.vlmcsd", package.seeall)
 
 function index()
     if not nixio.fs.access("/etc/config/vlmcsd") then
         return
     end
-    entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("vlmcsd), 2)
+    entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("vlmcsd"), 2)
 end
--- luci/vlmcsd/luasrc/model/cbi/vlmcsd.lua
+++ luci/vlmcsd/luasrc/model/cbi/vlmcsd.lua
@@ -1,10 +1,18 @@
+require("luci.sys")
+
 m=Map("vlmcsd", translate("vlmcsd"),
-    translate("Configure vlmcsd service."))
+    translate("Configure vlmcsd service (Windows/Office KMS activation service)."))
 
 s=m:section(TypedSection, "vlmcsd", translate("Settings"))
 s.annonymous=true
 s:option(Flag, "enabled", translate("Enable"))
 port =s:option(Value, "port", translate("Port"))
 port.rmempty = true
+s:option(Flag, "useepifile", translate("Use File"), translate("Use vlmcsd.ini file, not required"))
+
+local apply = luci.http.formvalue("cbi.apply")
+if apply then
+        io.popen("/etc/init.d/vlmcsd restart")
+end
 
 return m
